cd8d69791b26d39c5e6e370f7146aa7c2b581478
Sergei Kuznetsov <kuznetsov-s@haulmont.com>
2018-12-21 12:06:26 +0400

    fixed navbar width

--- js/navbar.js
+++ js/navbar.js
@@ -2,28 +2,31 @@
   // Fixed right navbar
   var sidebar = $(".blog .region-sidebar > section");
   var wrapper = $(".blog .region-content");
-  var sidebar_width = sidebar.width();
   var topBorder = 180;
   var bottomBorder = topBorder + wrapper.height() - sidebar.height() - 100;
   var images_loading = true;
+  var sidebar_width = sidebar.width();
+  if(sidebar_width > 283) sidebar_width = 283;
+  sidebar.css('width', sidebar_width + 'px');
   $(window).scroll(function(event){
     var st = $(this).scrollTop();
     if (st > topBorder && st < bottomBorder) {
       sidebar
         .css('position', 'fixed')
-        .css('width', sidebar_width + 'px')
         .css('margin-top', 0);
     } else if(st >= bottomBorder) {
-      sidebar.css('position', 'inherit');
-      sidebar.css('margin-top', bottomBorder - topBorder);
+      sidebar
+        .css('position', 'inherit')
+        .css('margin-top', bottomBorder - topBorder);
     } else {
-      sidebar.css('position', 'inherit');
-      sidebar.css('margin-top', 0);
+      sidebar
+        .css('position', 'inherit')
+        .css('margin-top', 0);
     }
   });
 
   $(window).resize(function() {
-    resizeNavbar();
+    //resizeNavbar();
   });
 
   // Waiting until all images is loaded
@@ -32,22 +35,22 @@
   });
 
   // Wait for all images loaded
-  resizeNavbarLoop();
+  navbarWidthLoop();
 
-  function resizeNavbarLoop() {
+  function navbarWidthLoop() {
     setTimeout(function() {
-      resizeNavbar();
-      if(images_loading) resizeNavbarLoop();
-    }, 1000);
+      bottomBorder = topBorder + wrapper.height() - sidebar.height() - 100;
+      if(images_loading) navbarWidthLoop();
+    }, 500);
   }
 
   function resizeNavbar() {
     sidebar_width = $(window).width() * 0.25;
     bottomBorder = topBorder + wrapper.height() - sidebar.height() - 100;
-    if(sidebar_width > 284) {
-      sidebar_width = 284;
+    if(sidebar_width > 283) {
+      sidebar_width = 283;
     } else {
-      sidebar_width = Math.round(sidebar_width);
+      sidebar_width = Math.floor(sidebar_width);
     }
   }
 })(jQuery);
