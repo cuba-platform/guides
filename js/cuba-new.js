c9e30bea80d388a3935ebff06fa5aad2c7c31893
Mario <mario@die-davids.de>
2019-09-25 08:13:57 +0200

    Built from 5eb91d2

--- js/cuba-new.js
+++ js/cuba-new.js
@@ -14,13 +14,4 @@
     $(this).parent().toggleClass('-expanded');
   });
 
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top - 80
-            }, 1500);
-        }
-    });
 }(jQuery);
